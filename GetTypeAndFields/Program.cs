82c364de024500178c830bc74e1cb6fb42d5d0f7
Eduardo Costa Rodrigues <edu.crod@gmail.com>
2021-05-11 23:22:26 -0300

    Add project files.

---
+++ GetTypeAndFields/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+namespace GetTypeAndFields
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HasASecret keeper = new HasASecret();
+
+            // Uncommenting this Console.WriteLine statement causes a compiler error: // 'HasASecret.secret' is inaccessible due to its protection level
+            // Console.WriteLine(keeper.secret);
+            // But we can still use reflection to get the value of the secret field
+
+            FieldInfo[] fields = keeper.GetType().GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+
+            foreach (FieldInfo field in fields) 
+            {
+                Console.WriteLine(field.GetValue(keeper));
+            }
+        }
+    }
+}
